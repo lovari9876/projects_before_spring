6543998bea4266912f0c54a0265f5ec9a3c11363
lovari <lovari9876@gmail.com>
2020-02-12 12:49:48 +0900

    projects_before_spring commit

---
+++ Control/Control.java
@@ -0,0 +1,27 @@
+﻿package Control;
+
+import java.util.Scanner;
+
+public class Control {
+    public static void main(String[] args) {
+
+	//정수를 입력 받아서
+	//양의 정수면 양수라 출력하고, 
+	//음의 정수면 음수라 출력하시오.
+
+	int integer;
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("정수를 입력하시오: ");
+	integer = scanner.nextInt();
+
+	if (integer > 0) {
+	    System.out.println("양수입니다.");
+	} else if (integer < 0) {
+	    System.out.println("음수입니다.");
+	} else {
+	    System.out.print("");
+	}
+	
+    }
+}
---
+++ Control/Control2.java
@@ -0,0 +1,26 @@
+﻿package Control;
+
+import java.util.Scanner;
+
+public class Control2 {
+    public static void main(String[] args) {
+
+	//성별을 입력받아
+	//M이면 남성, F이면 여성을 출력.
+	String gender;
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.print("성별을 입력하시오: ");
+	gender = scanner.next();
+
+	//equalsIgnoreCase() 메소드는 대소문자를 구분하지 않고 비교한다.
+	//equals() 메소드는 대소문자를 구분하여 비교하므로 
+	//원하는 비교 형태를 확인하고 선택하여 사용하기
+	if (gender.equalsIgnoreCase("M")) {
+	    System.out.println("남성");
+	} else if (gender.equalsIgnoreCase("F")) {
+	    System.out.println("여성");
+	}
+	
+    }
+}
